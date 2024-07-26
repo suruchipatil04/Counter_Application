ceff8233461e2a8d93dbc62feb3cfa08ef1eaeb8
suruchipatil04 <141062922+suruchipatil04@users.noreply.github.com>
2024-07-26 15:42:20 +0530

    File Upload

---
+++ scripts.js
@@ -0,0 +1,39 @@
+(function(){
+    const buttons =document.querySelectorAll(".btn-counter");
+    let count=0;
+
+    const counter =document.querySelector(".counter-machine");
+
+    const counterContainer=document.querySelector(".counter");
+
+    buttons.forEach(function(button)
+    {
+        button.addEventListener('click',function()
+        {
+            if(button.classList.contains("decrease-btn"))
+            {
+                count--;
+            }
+            else if(button.classList.contains("increase-btn"))
+            {
+                count++;
+            }
+            counter.textContent=count;
+
+            if(count>0)
+            {
+                counterContainer.classList.remove("negative");
+                counterContainer.classList.add("positive");
+            }
+            else if(count<0)
+            {
+                counterContainer.classList.add("negative");
+                counterContainer.classList.remove("positive");
+            }
+            else{
+                counterContainer.classList.remove("positive","negative")
+            }
+
+        })
+    })
+})();
